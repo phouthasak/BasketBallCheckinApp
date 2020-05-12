135254abb3314c7a47b95b17ae0fb4ba96a6289a
phouthasak douanglee <jphouthasakdouanglee@gmail.com>
2020-05-11 23:56:50 -0400

    Updated About me page info

--- frontend/src/components/resume/About.js
+++ frontend/src/components/resume/About.js
@@ -18,7 +18,7 @@ class About extends Component {
          <div className="three columns">
             <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
          </div>
-         <div className="nine columns main-col">
+         <div className="nine columns main-col text-left">
             <h2>About Me</h2>
 
             <p>{bio}</p>
@@ -33,7 +33,7 @@ class About extends Component {
                </div>
                <div className="columns download">
                   <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
+                     <a href={resumeDownload} className="button"><i className="fa fa-download" />Download Resume</a>
                   </p>
                </div>
             </div>
--- frontend/src/components/resume/Header.js
+++ frontend/src/components/resume/Header.js
@@ -5,10 +5,9 @@ class Header extends Component {
     if(this.props.data){
       var name = this.props.data.name;
       var occupation= this.props.data.occupation;
-      var description= this.props.data.description;
       var city= this.props.data.address.city;
       var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+        return <li key={network.name}><a href={network.url} target='_blank' rel='noopener noreferrer'><i className={network.className} /></a></li>
       })
     }
 
@@ -31,7 +30,7 @@ class Header extends Component {
         <div className="row banner">
           <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+            <h3>I am a <span>{occupation}</span> based in <span>{city}</span>. Currently employed by <span>JP Morgan Chase & CO.</span> specializing in <span>cloud base application development and microservices.</span></h3>
             <hr />
             <ul className="social">
               {networks}
@@ -40,7 +39,7 @@ class Header extends Component {
         </div>
 
         <p className="scrolldown">
-          <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+          <a className="smoothscroll" href="#about"><i className="icon-down-circle" /></a>
         </p>
       </header>
     );
