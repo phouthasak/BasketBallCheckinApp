1f4119bfb792d8115f9f86ffee310d480011d385
phouthasak douanglee <jphouthasakdouanglee@gmail.com>
2021-06-06 12:10:15 -0700

    Removed personal website stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Switch, Route } from 'react-router';
 import Basketball from './components/Basketball';
-import PersonalPage from './components/PersonalPage';
 import CreateEvent from './components/basketball/CreateEvent';
 import ViewPlayers from './components/basketball/ViewPlayers';
 import EditEvent from "./components/basketball/EditEvent";
@@ -13,8 +12,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path='/' component={PersonalPage}/>
-          <Route path='/basketBall' component={Basketball}/>
+          <Route exact path='/' component={Basketball}/>
           <Route path='/createEvent' component={CreateEvent}/>
           <Route path='/viewPlayers' component={ViewPlayers}/>
           <Route path='/editEvent/:id' component={(props) => <EditEvent eventId={props.match.params.id} />}/>
